7d106856def605300a63afec98b82de9daece4e4
ashwani8418 <68030476+ashwani8418@users.noreply.github.com>
2020-11-16 19:00:16 +0530

    code updated

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,18 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-    const [showPara, setShowPara] = useState(true);
+    const [showPara, setShowPara] = useState(false);
     return ( 
-    <div id = "main" >
-           <button id="click" onClick={() => setShowPara(false)}>Show Paragraph</button>
-            {showPara ? ( <p id = "para"> Hello, I 've learnt to use the full-stack evaluation tool. This makes me so happy</p>) : null} 
-    </div>
-);
-}
-
-export default App;
+        <div id="main">
+        <button id="click" onClick={() => setShowPara(true)}>Show Paragraph</button>
+        {showPara ? (
+          <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+      </p>
+        ): null}
+      </div>
+    );
+  }
+  
+  
+  export default App;
