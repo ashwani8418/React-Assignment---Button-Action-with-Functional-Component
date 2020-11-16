7ddfe05d2723fbd9b3786ab6ca995122cda4103b
ashwani8418 <68030476+ashwani8418@users.noreply.github.com>
2020-11-16 18:28:34 +0530

    button setShowPara -true update

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,12 @@ import "./../styles/App.css";
 
 function App() {
     const [showPara, setShowPara] = useState(false);
-    return ( <div id = "main" >
-            <button id = "click" onClick = {() => setShowPara(!showPara) }> Toggle Para </button> {showPara ? ( <p id = "para"> Hello, I 've learnt to use the full-stack evaluation tool. This makes me so happy</p>) : null} 
-                </div>
-                );
-            }
+    return ( 
+    <div id = "main" >
+            <button id = "click" onClick = {() => setShowPara(true) }> Show Para </button> 
+            {showPara ? ( <p id = "para"> Hello, I 've learnt to use the full-stack evaluation tool. This makes me so happy</p>) : null} 
+    </div>
+);
+}
 
-
-            export default App;
+export default App;
